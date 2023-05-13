61ffce8e9244d351f0a2524cbac67e56e82d66a4
Adithyan PB <118650366+Adithyan88pb@users.noreply.github.com>
2023-05-13 12:43:00 +0530

    Add files via upload

---
+++ java.js
@@ -0,0 +1,17 @@
+let menu=document.querySelector('#menu-icon');
+let navmenu=document.querySelector('.navmenu');
+
+menu.onclick=() =>{
+    menu.classList.toggle('bx-x');
+    navmenu.classList.toggle('open');
+}
+
+const sr=ScrollReveal({
+ distance:'65px',
+ duration:2600,
+ delay:450,
+ reset:true
+})
+
+sr.reveal('.text' ,{delay:200, origin:'top'});
+sr.reveal('.main-img', {delay:500, origin:'top'});
